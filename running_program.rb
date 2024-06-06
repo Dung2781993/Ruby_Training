93c938755b3f293de8a5a05ecbb2715f71bdb7de
JasonLe <Dung2781993@gmail.com>
2024-06-07 01:44:52 +1000

    String in Ruby

--- running_program.rb
+++ running_program.rb
@@ -1,3 +1,11 @@
 puts 2+2
 
-puts "Hello, Mama"
+puts "Hello" + " " +  "Mama"
+
+
+puts "Hello, world".sub("Hello", "Good morning")
+
+
+puts "Hello, world".gsub("o", "s")
+
+puts "Hello, world".include?("world")
